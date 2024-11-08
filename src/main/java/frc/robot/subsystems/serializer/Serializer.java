9f0ea9b96cc143e515bc30d6992db8bfe2183478
TigerBots <admin@9072.team>
2024-11-07 19:41:31 -0500

    instantiate HAL

--- src/main/java/frc/robot/subsystems/serializer/Serializer.java
+++ src/main/java/frc/robot/subsystems/serializer/Serializer.java
@@ -1,5 +1,18 @@
 package frc.robot.subsystems.serializer;
 
-public class Serializer {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.serializer.decisionrollers.DecisionRollersHalI;
+import frc.robot.subsystems.serializer.feeder.FeederRollersHalI;
+import frc.robot.subsystems.serializer.sensors.SensorHalI;
+
+public class Serializer extends SubsystemBase{
+    private final DecisionRollersHalI m_decisionRollers;
+    private final FeederRollersHalI m_feederRollers;
+    private final SensorHalI m_sensors;
+
+    public Serializer(DecisionRollersHalI decisionRollers, FeederRollersHalI feederRollers, SensorHalI sensors) {
+        m_decisionRollers = decisionRollers;
+        m_feederRollers = feederRollers;
+        m_sensors = sensors;
+    }
 }
