d153c40333a1479eb2f3ce0fe44ae0492fd4b305
TigerBots <admin@9072.team>
2024-11-07 19:46:25 -0500

    intake command

--- src/main/java/frc/robot/subsystems/serializer/Serializer.java
+++ src/main/java/frc/robot/subsystems/serializer/Serializer.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.serializer;
 
+import org.growingstems.measurements.Measurements.Voltage;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.serializer.decisionrollers.DecisionRollersHalI;
 import frc.robot.subsystems.serializer.feeder.FeederRollersHalI;
@@ -14,5 +18,21 @@ public class Serializer extends SubsystemBase{
         m_decisionRollers = decisionRollers;
         m_feederRollers = feederRollers;
         m_sensors = sensors;
+    }
+
+    private void stop() {
+        m_decisionRollers.brake();
+        m_feederRollers.brake();
+    }
+
+    private void startIntake() {
+        m_feederRollers.setPower(new Voltage(4));
+    }
+
+    public Command intake() {
+        return Commands.sequence(
+            run(this::startIntake),
+            Commands.waitUntil(m_sensors::getRearBreak)
+        ).finallyDo(this::stop);
     }
 }
