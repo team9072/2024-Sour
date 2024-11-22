1e16f8aa4b5273b04c52d5d580e0940044ab3fb3
TigerBots <admin@9072.team>
2024-11-22 17:22:15 -0500

    actually undo robot changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,31 +2,30 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
- package frc.robot;
+package frc.robot;
 
- import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Command;
 
- public class Robot {
-     public static final String k_canivoreCan = "CANivore";
- 
-     private Command m_autoCommand = null;
- 
-     public Robot() {
-         configureBindings();
-     }
- 
-     public void update() {}
- 
-     private void configureBindings() {}
- 
-     public void updateAutoCommand() {}
- 
-     public Command getAutonomousUCommand() {
-         if (m_autoCommand == null) {
-             updateAutoCommand();
-         }
- 
-         return m_autoCommand;
-     }
- }
- 
+public class Robot {
+    public static final String k_canivoreCan = "CANivore";
+
+    private Command m_autoCommand = null;
+
+    public Robot() {
+        configureBindings();
+    }
+
+    public void update() {}
+
+    private void configureBindings() {}
+
+    public void updateAutoCommand() {}
+
+    public Command getAutonomousUCommand() {
+        if (m_autoCommand == null) {
+            updateAutoCommand();
+        }
+
+        return m_autoCommand;
+    }
+}
