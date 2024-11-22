2acc64ce2241c251d80ba43360c6c7e7e58258e5
TigerBots <admin@9072.team>
2024-11-21 19:34:53 -0500

    Tune kg

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,41 +2,42 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-package frc.robot;
+ package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.intake.deploy.IntakeDeployHal;
-
-public class Robot {
-    public static final String k_canivoreCan = "CANivore";
-
-    public final CommandXboxController m_driverController= new CommandXboxController(0);
-    public final IntakeDeployHal m_intake;
-
-    private Command m_autoCommand = null;
-
-    public Robot() {
-        configureBindings();
-        m_intake = new IntakeDeployHal();
-    }
-
-    public void update() {}
-
-    private void configureBindings() {
-        m_driverController.b().onTrue(Commands.runOnce(() -> {
-            m_intake.m_motor.setPosition(0);
-        }));
-    }
-
-    public void updateAutoCommand() {}
-
-    public Command getAutonomousUCommand() {
-        if (m_autoCommand == null) {
-            updateAutoCommand();
-        }
-
-        return m_autoCommand;
-    }
-}
+ import edu.wpi.first.wpilibj2.command.Command;
+ import edu.wpi.first.wpilibj2.command.Commands;
+ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+ import frc.robot.subsystems.intake.deploy.IntakeDeployHal;
+ 
+ public class Robot {
+     public static final String k_canivoreCan = "CANivore";
+ 
+     public final CommandXboxController m_driverController= new CommandXboxController(0);
+     public final IntakeDeployHal m_intake;
+ 
+     private Command m_autoCommand = null;
+ 
+     public Robot() {
+         configureBindings();
+         m_intake = new IntakeDeployHal();
+     }
+ 
+     public void update() {}
+ 
+     private void configureBindings() {
+         m_driverController.b().onTrue(Commands.runOnce(() -> {
+             m_intake.m_motor.setPosition(0);
+         }));
+     }
+ 
+     public void updateAutoCommand() {}
+ 
+     public Command getAutonomousUCommand() {
+         if (m_autoCommand == null) {
+             updateAutoCommand();
+         }
+ 
+         return m_autoCommand;
+     }
+ }
+ 
